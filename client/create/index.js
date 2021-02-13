88b830ff3f6c252ec3ad7b1b5c7ebde2e37a0d77
Patsomir <pnn246@gmail.com>
2021-02-13 22:24:43 +0200

    Add navigation to editor

--- client/create/index.js
+++ client/create/index.js
@@ -1,5 +1,20 @@
 import { buildDefaultToolbar } from '../utils/toolbar.js';
+import { moveTo } from '../utils/navigation.js';
+
+const WIDTH_ID = 'width';
+const HEIGHT_ID = 'height';
+const TYPE_ID = 'editor-type';
+const OPEN_BUTTON_ID = 'open-button';
+const EDITOR_PATH = '/editor';
+
+function openEditor() {
+    const width = document.getElementById(WIDTH_ID).value;
+    const height = document.getElementById(HEIGHT_ID).value;
+    const type = document.getElementById(TYPE_ID).value;
+    moveTo(`${EDITOR_PATH}?width=${width}&height=${height}&type=${type}`);
+}
 
 window.addEventListener('load', () => {
     buildDefaultToolbar();
+    document.getElementById(OPEN_BUTTON_ID).addEventListener('click', openEditor);
 });
