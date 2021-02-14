c03edb5bf0dc6619150f4c4feada64c0b3f1a028
Patsomir <pnn246@gmail.com>
2021-02-14 21:16:59 +0200

    Add text input in create

--- client/create/index.js
+++ client/create/index.js
@@ -7,8 +7,9 @@ const HEIGHT_ID = 'height';
 const TYPE_ID = 'editor-type';
 const BLANK_OPTION_ID = 'blank-option';
 const IMAGE_OPTION_ID = 'image-option';
-const JSON_OPTION_ID = 'json-option';
+const TEXT_OPTION_ID = 'text-option';
 const UPLOAD_IMAGE_BUTTON_ID = 'upload-image-button';
+const UPLOAD_TEXT_BUTTON_ID = 'upload-text-button';
 const ART_CONTAINER_ID = 'art-container';
 const INNER_CONTAINER_ID = 'inner-container';
 const OPEN_BUTTON_ID = 'open-button';
@@ -65,36 +66,57 @@ function displayAsciiFromImage(event){
     }
 };
 
+function displayAsciiFromText(event) {
+    if(event.target.files.length > 0) {
+        event.target.files[0].text()
+        .then(text => {
+            loadAsciiArt(text);
+            localStorage.setItem(INIT_TEMPLATE_KEY, text);
+        })
+        .catch(() => console.error('Error occured while importing file'));
+    }
+                
+}
+
 function loadAsciiArt(asciiArt) {
     document.getElementById(ART_CONTAINER_ID).style.display = 'flex';
-    document.getElementById(INNER_CONTAINER_ID).style.display = 'block';    
     let innerContainer = document.getElementById(INNER_CONTAINER_ID);
+    innerContainer.style.display = 'block';
     innerContainer.textContent = asciiArt;
 }
 
 function openBlankInput(){
-    document.getElementById(UPLOAD_IMAGE_BUTTON_ID).style.display = 'none';
-    document.getElementById(ART_CONTAINER_ID).style.display = 'none';
-    document.getElementById(INNER_CONTAINER_ID).style.display = 'none';    
+    resetInputState();
 }
 
 function openImageInput(){
+    resetInputState();
     document.getElementById(UPLOAD_IMAGE_BUTTON_ID).style.display = 'block';
-    document.getElementById(ART_CONTAINER_ID).style.display = 'none';
-    document.getElementById(INNER_CONTAINER_ID).style.display = 'none';    
 }
 
-function openJSONInput(){
-    document.getElementById(UPLOAD_IMAGE_BUTTON_ID).style.display = 'none';
+function openTextInput(){
+    resetInputState();
+    document.getElementById(UPLOAD_TEXT_BUTTON_ID).style.display = 'block';
+}
+
+function resetInputState() {
+    const imageButton = document.getElementById(UPLOAD_IMAGE_BUTTON_ID);
+    imageButton.style.display = 'none';
+    imageButton.value = null;
+    const textButton = document.getElementById(UPLOAD_TEXT_BUTTON_ID);
+    textButton.style.display = 'none';
+    textButton.value = null;
     document.getElementById(ART_CONTAINER_ID).style.display = 'none';
-    document.getElementById(INNER_CONTAINER_ID).style.display = 'none';    
+    document.getElementById(INNER_CONTAINER_ID).style.display = 'none';
+    localStorage.removeItem(INIT_TEMPLATE_KEY); 
 }
 
 window.addEventListener('load', () => {
     buildDefaultToolbar();
     document.getElementById(OPEN_BUTTON_ID).addEventListener('click', openEditor);
     document.getElementById(UPLOAD_IMAGE_BUTTON_ID).addEventListener('change', displayAsciiFromImage);
+    document.getElementById(UPLOAD_TEXT_BUTTON_ID).addEventListener('change', displayAsciiFromText);
     document.getElementById(BLANK_OPTION_ID).addEventListener('click', openBlankInput);
     document.getElementById(IMAGE_OPTION_ID).addEventListener('click', openImageInput);
-    document.getElementById(JSON_OPTION_ID).addEventListener('click', openJSONInput);
+    document.getElementById(TEXT_OPTION_ID).addEventListener('click', openTextInput);
 });
