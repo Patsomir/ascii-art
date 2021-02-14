9b372ef5a6f2864b31737a89970ebc6c4693b55e
nnyx7 <nnyx77@gmail.com>
2021-02-14 04:09:35 +0200

    Add art service
    It manages POST and GET requests to the arts database

--- server/validation.php
+++ server/validation.php
@@ -17,6 +17,24 @@
         }
     }
 
+    function validateInteger($num, $displayName = 'Value') {
+        if(!is_int($num)) {
+            return "$displayName must be an integer";
+        }
+    }
+
+    function validateNonNegative($num, $displayName = 'Value') {
+        if($num < 0) {
+            return "$displayName must be non negative";
+        }
+    }
+
+    function validatePositive($num, $displayName = 'Value') {
+        if($num <= 0) {
+            return "$displayName must be positive";
+        }
+    }
+
     function pushIfError(&$arr, $value) {
         if(!is_null($value)) {
             array_push($arr, $value);
