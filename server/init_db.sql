eb577be3c7749204eb175001367144d5d35fcdab
Patsomir <pnn246@gmail.com>
2021-02-14 10:10:51 +0200

    Add likes table to db

--- server/init_db.sql
+++ server/init_db.sql
@@ -26,4 +26,17 @@ CREATE TABLE arts
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+CREATE TABLE likes
+(
+    id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    art_id int NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (art_id) REFERENCES arts(id),
+    UNIQUE KEY (user_id, art_id)
+);
+
 COMMIT;
